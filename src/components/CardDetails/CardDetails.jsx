98a1b8f08d592545360541e7cb8356532db3ae57
Md. Arif Ul Islam <arifulislam161148@gmail.com>
2023-05-27 18:08:08 +0600

    chef section 6 card done

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -5,22 +5,28 @@ const CardDetails = ({ chef }) => {
     id,
     likes,
     name,
-    number_of_recepies,
+    numbers_of_recipes,
     picture,
     recipe_details,
     years_of_experience,
   } = chef;
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img className="w-11/12 h-48 rounded" src={picture} alt="Shoes" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-              <p>Years Of Experiences: {years_of_experience}</p>
-           
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+    <div className="flex justify-center items-center">
+      <div className="card w-96 bg-base-100">
+        <figure>
+          <img className="w-11/12 h-48 rounded-lg" src={picture} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>Years Of Experiences: {years_of_experience}</p>
+          <p>Number of Recipes: {numbers_of_recipes}</p>
+          <p>Likes: {likes}</p>
+
+          <div className="w-full justify-center items-center">
+            <button className="btn btn-primary w-full rounded-lg">
+              Buy Now
+            </button>
+          </div>
         </div>
       </div>
     </div>
