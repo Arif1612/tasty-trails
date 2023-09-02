5eaa76b09df25b4148f06c19020e98de2a335df0
Arif <arifulislam161148@gmail.com>
2023-09-02 17:26:40 +0600

    footer section updated

--- src/components/Home/OurStrength.jsx
+++ src/components/Home/OurStrength.jsx
@@ -1,35 +1,41 @@
-import React from 'react';
+import React from "react";
+import strength from "../../assets/featured-food/strength.jpg";
 
 const OurStrength = () => {
-    return (
+  return (
+    <div className="mb-20">
+      {/* our  Strength */}
+      <h3 className="text-5xl font-bold text-center mt-14 mb-14">
+        Our Strength
+      </h3>
+      {/* main */}
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        {/* left */}
+        <div className="items-center  my-auto">
+          <div className="grid grid-cols-1 gap-8 mb-10   ">
+            {/* 1 */}
+            <div className="text-start font-bold text-xl">
+              <ul className="list-disc list-inside">
+                <li>Quality of Food</li>
+                <li>Food Safety</li>
+                <li>Quality Ingredients</li>
+                <li>Menu Transparency</li>
+                <li>Customer Service </li>
+                <li>Timely Service </li>
+                <li>Reservation and Waiting List Management</li>
+                <li>Pricing and Billing Transparenc</li>
+              </ul>
+            </div>
+           
+          </div>
+        </div>
+        {/* right */}
         <div>
-    {/* our  Strength */}
-        <h3 className="text-5xl font-bold text-center mt-14 mb-14">
-          Our Strength
-        </h3>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mb-10 ">
-          {/* 1 */}
-          <div className="text-start font-bold">
-            <ul className="list-decimal list-inside">
-              <li>Quality of Food</li>
-              <li>Food Safety</li>
-              <li>Quality Ingredients</li>
-              <li>Menu Transparency</li>
-            </ul>
-          </div>
-          {/* 2 */}
-          <div className="text-start font-bold">
-            <ul className="list-decimal list-inside">
-              <li>Customer Service </li>
-              <li>Timely Service </li>
-              <li>Reservation and Waiting List Management</li>
-              <li>Pricing and Billing Transparenc</li>
-            </ul>
-          </div>
+          <img className=" rounded-xl " src={strength} alt="" />
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default OurStrength;
